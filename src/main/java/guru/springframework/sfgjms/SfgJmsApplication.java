b93d2883ba2649a1350c3f33e97b7c758f35bf2f
Rahul <rsingh33@buffalo.edu>
2020-05-06 22:30:16 -0400

    Configured Task Executor and Ebanled Scheduling

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,12 +1,25 @@
 package guru.springframework.sfgjms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 public class SfgJmsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        ActiveMQServer server = new ActiveMQServerImpl(new ConfigurationImpl()
+        .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+        .addAcceptorConfiguration("invm","vm://0"));
+
+        server.start();
         SpringApplication.run(SfgJmsApplication.class, args);
     }
 
