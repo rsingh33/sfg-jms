cc0126b202c1cd24f588fbbb176ff38a316c161f
Rahul <rsingh33@buffalo.edu>
2020-05-06 22:49:00 -0400

    Set up JMS config

---
+++ src/main/java/guru/springframework/sfgjms/config/JMSConfig.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JMSConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter mappingJackson2MessageConverter =
+                new MappingJackson2MessageConverter();
+        mappingJackson2MessageConverter.setTargetType(MessageType.TEXT);
+        mappingJackson2MessageConverter.setTypeIdPropertyName("_type");
+        return mappingJackson2MessageConverter;
+    }
+}
